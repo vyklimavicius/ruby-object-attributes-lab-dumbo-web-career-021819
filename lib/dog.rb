5f813313a46332ac82f0f1c7566894fd6319326b
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-20 03:50:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
-  def initialize(dog_name)
-    @name = dog_name
-  end 
+  # def initialize(dog_name)
+  #   @name = dog_name
+  # end 
   
   def name=(new_dog_name) # setter 
     @name = new_dog_name
